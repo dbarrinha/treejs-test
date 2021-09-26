1515b6f7fb4401efc51c257ecd548de16a58599e
dbarrinha <dbarrinha@hotmail.com>
2021-09-26 14:50:46 -0300

    feat: add a  texture to the sphere

--- src/molds.js
+++ src/molds.js
@@ -1,13 +1,17 @@
 import * as THREE from 'three'
+import Texture from '../static/textures/NormalMap.png'
 
 export const getSphere = () => {
     const geometry = new THREE.SphereBufferGeometry(0.5, 32, 32 )
+    const textureLoader = new THREE.TextureLoader()
+    const normalTexture = textureLoader.load(Texture)
     const material = new THREE.MeshStandardMaterial( 
         { 
             color: "#e78", 
             metalness: 0.2,
             roughness: 0.3, 
-            flatShading: 10
+            flatShading: 10,
+            normalMap: normalTexture
         } 
     );
     return new THREE.Mesh( geometry, material );
