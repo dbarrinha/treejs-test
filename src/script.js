3d55acc9b6084034116615acb9093ada0da38588
dbarrinha <dbarrinha@hotmail.com>
2021-09-26 14:25:45 -0300

    refactor: remove some comments

--- src/script.js
+++ src/script.js
@@ -1,21 +1,13 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 import {getSphere} from './molds'
-// Debug
-const gui = new dat.GUI()
 
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
 const scene = new THREE.Scene()
 
 const sphere = getSphere()
 scene.add(sphere)
-
-// Lights
 
 const pointLight = new THREE.PointLight(0xffff, 0.6)
 pointLight.position.x = 2
@@ -23,9 +15,6 @@ pointLight.position.y = 4
 pointLight.position.z = 10
 scene.add(pointLight)
 
-/**
- * Sizes
- */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -46,33 +35,18 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
 
 const clock = new THREE.Clock()
 
@@ -81,18 +55,12 @@ const tick = () =>
 
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
+    // Update object
     sphere.rotation.y = .5 * elapsedTime
     sphere.rotation.x = .5 * elapsedTime
     sphere.rotation.z = .5 * elapsedTime
 
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
     renderer.render(scene, camera)
-
-    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
