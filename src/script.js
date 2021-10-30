ef35440eab2ee209dcff627e5fff4c6a05abbd4d
dbarrinha <dbarrinha@hotmail.com>
2021-10-30 14:21:43 -0300

    feat: add a parallax effect

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import {getSphere} from './molds'
 import * as dat from 'dat.gui'
 
-const gui = new dat.GUI()
+//const gui = new dat.GUI()
 
 function addGuiLight(folderRef, light){
     folderRef.add(light.position, 'y')
@@ -38,8 +38,8 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
-const light1 = gui.addFolder('Light 1')
-addGuiLight(light1, pointLight)
+// const light1 = gui.addFolder('Light 1')
+// addGuiLight(light1, pointLight)
 
 //Light 2
 const pointLight2 = new THREE.PointLight(0xff0000, 2)
@@ -47,32 +47,32 @@ pointLight2.position.set(-1.86,1,-1.65)
 pointLight2.intensity = 1
 scene.add(pointLight2)
 
-const light2 = gui.addFolder('Light 2')
-addGuiLight(light2, pointLight2)
+// const light2 = gui.addFolder('Light 2')
+// addGuiLight(light2, pointLight2)
 
-const pointLioghtHelper = new THREE.PointLightHelper(pointLight2, 1)
-scene.add(pointLioghtHelper)
+// const pointLioghtHelper = new THREE.PointLightHelper(pointLight2, 1)
+// scene.add(pointLioghtHelper)
 
 //Light 3
-const pointLight3 = new THREE.PointLight(0xff0000, 2)
-pointLight3.position.set(-1.86,1,-1.65)
-pointLight3.intensity = 1
+const pointLight3 = new THREE.PointLight(0xe1ff, 2)
+pointLight3.position.set(2.13,-3, -1.98)
+pointLight3.intensity = 6.8
 scene.add(pointLight3)
 
-const light3 = gui.addFolder('Light 3')
-addGuiLight(light3, pointLight3)
+// const light3 = gui.addFolder('Light 3')
+// addGuiLight(light3, pointLight3)
 
-const light3Color = {
-    color: 0xff0000
-}
+// const light3Color = {
+//     color: 0xe1ff
+// }
 
-light3.addColor(light3Color, 'color')
-    .onChange(()=>{
-        pointLight3.color.set(light3Color.color)
-    })
+// light3.addColor(light3Color, 'color')
+//     .onChange(()=>{
+//         pointLight3.color.set(light3Color.color)
+//     })
 
-const pointLioghtHelper2 = new THREE.PointLightHelper(pointLight3, 1)
-scene.add(pointLioghtHelper2)
+// const pointLioghtHelper2 = new THREE.PointLightHelper(pointLight3, 1)
+// scene.add(pointLioghtHelper2)
 
 const sizes = {
     width: window.innerWidth,
@@ -97,7 +97,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.position.z = 2.5
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
@@ -107,17 +107,40 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+let mouseX = 0
+let mouseY = 0
+
+let targetX = 0
+let targetY = 0
+
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+
+function onDocumentMouseMove(event){
+    mouseX = (event.clientX - windowHalfX)
+    mouseY = (event.clientY - windowHalfY)
+}
+
+function onDocumentScroll(event){
+    sphere.position.y = window.scrollY * .001
+}
+
+
+document.addEventListener('mousemove', onDocumentMouseMove)
+document.addEventListener('scroll', onDocumentScroll)
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
 
+    targetX = mouseX * .001;
+    targetY = mouseY * .001;
+    
     const elapsedTime = clock.getElapsedTime()
-
-    // Update object
     sphere.rotation.y = .5 * elapsedTime
-    //sphere.rotation.x = .5 * elapsedTime
-    //sphere.rotation.z = .5 * elapsedTime
+    sphere.rotation.y += 0.1 * (targetX - sphere.rotation.y)
+    sphere.rotation.x += 0.1 * (targetY - sphere.rotation.x)
+    sphere.position.z += 0.1 * (targetY - sphere.rotation.x)
 
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
